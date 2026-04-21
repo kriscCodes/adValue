677e326b210ed78fcbde987d40fc2d4c37fab1b2
c0mpsc1Art1st <compsciartist@gmail.com>
2026-04-21 13:09:09 -0400

    Deleted everything

--- adValue/frontend/app/(tabs)/explore.tsx
+++
@@ -1,6 +0,0 @@
-import { Platform } from 'react-native';
-
-import ExploreNative from './explore.native';
-import ExploreWeb from './explore.web';
-
-export default Platform.OS === 'web' ? ExploreWeb : ExploreNative;
