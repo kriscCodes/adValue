1ceeadbb1cb418ff6287e29bc9cb187e1bab5903
Faikar Herzaman <f.fazazaman@gmail.com>
2026-05-01 01:30:24 -0400

    refactor settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k6t^x&0&knc5hd($is1xd=hcl&l3ez4^^pjmh_8%-15cqri@55'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
