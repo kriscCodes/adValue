9d5abe7fcac32591e01cd7fcb1ac126c41216e57
kriscCodes <110057928+kriscCodes@users.noreply.github.com>
2026-05-12 15:54:12 -0400

    adjusting CORS

--- backend/settings.py
+++ backend/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
